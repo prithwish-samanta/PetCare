ce1a129c3f44d9152ac2da51f77839138471b59c
Prithwish Samanta <123155283+prithwish-samanta@users.noreply.github.com>
2025-02-14 10:02:11 +0000

    monolith api: project setup

---
+++ petcare-monolithic-rest-api/src/test/java/dev/prithwish/petcare_monolithic_rest_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prithwish.petcare_monolithic_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
