def7ca6536a377d8f1149ccde92170ce3d12cf07
prithwish <wprith@gmail.com>
2025-02-25 23:11:55 +0530

    monolith api: add loading spinner in ui

---
+++ petcare-monolithic-rest-api/client/src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+function LoadingSpinner() {
+    return (
+        <div className="text-center m-3">
+            <div className="spinner-grow text-primary fs-1" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    );
+
+}
+
+export default LoadingSpinner;
--- petcare-monolithic-rest-api/client/src/components/owners/OwnersPage.tsx
+++ petcare-monolithic-rest-api/client/src/components/owners/OwnersPage.tsx
@@ -4,22 +4,27 @@ import {IOwner} from "../../types";
 import {ApiManager} from "../../api-manager/ApiManager.ts";
 import OwnerInformation from "./OwnerInformation.tsx";
 import PetsTable from "./PetsTable.tsx";
+import LoadingSpinner from "../common/LoadingSpinner.tsx";
 
 function OwnersPage() {
     const {ownerId} = useParams();
     const [owner, setOwner] = useState<IOwner | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
         const loadOwnerDetails = async () => {
             if (!ownerId) return;
             const ownerIdNumber = Number(ownerId);
             setError(null);
+            setLoading(true);
             try {
                 const data = await ApiManager.getOwnerById(ownerIdNumber);
                 setOwner(data);
+                setLoading(false);
             } catch (err: any) {
                 setError(err.message);
+                setLoading(false);
             }
         };
 
@@ -28,6 +33,7 @@ function OwnersPage() {
     return (
         <section className="container my-3">
             <h2 className="text-center mb-3">Owner Details</h2>
+            {loading && <LoadingSpinner/>}
             {error && <p className="alert alert-danger text-center">{error}</p>}
             {owner && <OwnerInformation owner={owner}/>}
             {owner && <PetsTable ownerId={owner.id} pets={owner.pets}/>}
