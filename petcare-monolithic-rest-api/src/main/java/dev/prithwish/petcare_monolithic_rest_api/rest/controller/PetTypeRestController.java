754ac33855851bd43e37e902e5b6d2674518068d
prithwish <wprith@gmail.com>
2025-03-01 12:35:39 +0530

    monolith api: add cross-origin in pet type controller and config port for prod env

--- petcare-monolithic-rest-api/src/main/java/dev/prithwish/petcare_monolithic_rest_api/rest/controller/PetTypeRestController.java
+++ petcare-monolithic-rest-api/src/main/java/dev/prithwish/petcare_monolithic_rest_api/rest/controller/PetTypeRestController.java
@@ -8,16 +8,14 @@ import dev.prithwish.petcare_monolithic_rest_api.service.ClinicService;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Locale;
 
 @RestController
 @RequestMapping("/pettypes")
+@CrossOrigin(origins = "*")
 public class PetTypeRestController {
     private final ClinicService clinicService;
     private final MessageSource messageSource;
